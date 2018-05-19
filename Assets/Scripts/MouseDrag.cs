e54a7506aed1bb39c60e2b73a189b89c051cfd6f
Tejas Shah <tejas4197@hotmail.com>
2018-05-19 16:05:02 -0400

    added materials.
    Started working on the physics.

---
+++ Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDrag: MonoBehaviour {
+
+	 public float distance = 5;
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
