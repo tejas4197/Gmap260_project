94ba4525cf45f0a2677686768022a32c840356bf
Tejas Shah <tejas4197@hotmail.com>
2018-05-30 02:13:15 -0400

    Clean up, added colliders, added enemy
    Cleaned up the hierarchy. All object pipes are children of a pipe
    parent. both walls full under wall game object. Reworked colliders to 3d
    colliders of the platforms, added directional light to the scene and
    added some enemies in the level.

---
+++ Assets/Scripts/BallForce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallForce : MonoBehaviour {
+    public Rigidbody rb;
+    public float force;
+
+	// Use this for initialization
+	void Start () {
+        rb.AddForce(0, force, 0, ForceMode.VelocityChange);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
