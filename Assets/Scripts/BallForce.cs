2f13d71f6a7099acb035d804ea951dd2c9839bfc
Wills-M <wmm38@drexel.edu>
2018-05-31 12:14:42 -0400

    Fixed a bug with trampoline animation as well as one that was causing the trampoline to not collide with the ball anymore

--- Assets/Scripts/BallForce.cs
+++ Assets/Scripts/BallForce.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallForce : MonoBehaviour {
-    public Rigidbody rb;
+    public Rigidbody2D rb;
     public float force;
 
 	// Use this for initialization
 	void Start () {
-        rb.AddForce(0, force, 0, ForceMode.VelocityChange);
+        rb.AddForce(new Vector2(0, force), ForceMode2D.Impulse);
 
     }
 
