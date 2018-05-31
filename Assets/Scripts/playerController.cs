9742a734e89fa95d43367b707530562fc92581d3
Wills-M <wmm38@drexel.edu>
2018-05-31 11:54:28 -0400

    Scene resets of ball falls of camera

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerController : MonoBehaviour
 {
+    public Camera mainCamera;
 
     // Use this for initialization
     void Start()
@@ -14,7 +16,13 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        Vector3 ballInCameraPos = mainCamera.WorldToViewportPoint(gameObject.transform.position);
+
+        //If ball is not in camera
+        if (ballInCameraPos.x < 0 || ballInCameraPos.x > 1 || ballInCameraPos.y < 0 || ballInCameraPos.y > 1)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
     }
 
     void OnCollisionEnter(Collision collision)
