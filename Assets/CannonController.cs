6e4c6ff5160b852fbb3f39a2883bef2c5bb46d24
Wills-M <wmm38@drexel.edu>
2018-06-13 10:23:57 -0400

    Added working cannon enemy prefab

---
+++ Assets/CannonController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour {
+
+    public GameObject Cannonball;
+    public float RepeatRate;
+    public float CannonballSpeed;
+
+    Vector3 Placement;
+    Vector3 Velocity;
+
+	// Use this for initialization
+	void Start () {
+        float xOffset = gameObject.transform.rotation.y == 0 ? 2 : -2;
+        Placement = gameObject.transform.position;
+        Placement.y += 2.5f;
+        Placement.x += xOffset;
+
+        Velocity = Placement - (gameObject.transform.position + new Vector3(0, 1));
+        Velocity = Velocity.normalized;
+
+        InvokeRepeating("LaunchCannonball", 0.5f, RepeatRate);
+	}
+	
+	void LaunchCannonball()
+    {
+        GameObject instantiatedCannonball = Instantiate(Cannonball, Placement, Quaternion.identity);
+        Rigidbody rb = instantiatedCannonball.GetComponent<Rigidbody>();
+        rb.AddForce(Velocity * CannonballSpeed);
+    }
+}
