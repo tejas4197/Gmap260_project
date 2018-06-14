6b379ca83ed7686b63a0f1f9e4a40b601b442605
Gabe Scull <gss46@drexel.edu>
2018-06-13 23:46:43 -0400

    Menu scene transition script

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    // Gets next scene when start is pressed
+	public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Quits the game when exit is pressed
+    public void QuitGame()
+    {
+        Debug.Log("The Game Has Quit");
+        Application.Quit();
+    }
+}
